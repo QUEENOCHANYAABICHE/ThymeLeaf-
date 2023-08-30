5ad44914718598972b44412fda0a0192bd256d31
QUEENOCHANYAABICHE <abichequeen@gmail.com>
2023-08-30 18:40:59 +0100

    update

---
+++ src/main/java/com/example/thymeleafdemo/Employee.java
@@ -0,0 +1,46 @@
+package com.example.thymeleafdemo;
+
+import jakarta.persistence.Entity;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+
+//@NoArgsConstructor
+//@Entity
+public class Employee {
+
+    private String firstName;
+    private String lastName;
+    private String email;
+
+
+    public Employee(String firstName, String lastName, String email) {
+        super();
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
